22caffcded65b8ce7902c318e711ad89e64474b6
Lydia B <lydiabaik@gmail.com>
2019-11-26 00:56:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"
-        },
-      :matriarch => {name: "Lady Montague", age: "54"
-        },
-      :hero => {name: "Romeo", age: "15"        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15"},
       :hero_friends => []
    },
    :capulet => {
