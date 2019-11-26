00867c7913e22224cf25f179ba7d15a15f2e7d58
Lydia B <lydiabaik@gmail.com>
2019-11-26 00:38:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,15 +15,8 @@ def first_challenge
          :status => "alive"
        },
        :hero_friends => [
-         {
-           :name => "Benvolio",
-           :age => "17"
-           :attitude => "worried"
-         },
-         {
-           name: "Mercutio",
-           age: "18",
-           attitude: "hot-headed"
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"
          }
        ]
      },
@@ -42,17 +35,12 @@ def first_challenge
          :status => "alive"
        },
        :heroine_friends => [
-         {
-         :name => "Steven",
+      {:name => "Steven",
          :age => "30",
-         :attitude => "confused"
-       },
-       {
-         name: "Nurse",
+         :attitude => "confused"},
+       {name: "Nurse",
          age: "44",
-         attitude: "worried"
-       }
-       ]
+         attitude: "worried"} ]
      }
    }
 end
