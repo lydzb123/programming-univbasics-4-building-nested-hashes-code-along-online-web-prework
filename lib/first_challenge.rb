bc9ac669d0906e648b508f44dab80b6018d1c992
Lydia B <lydiabaik@gmail.com>
2019-11-26 00:41:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -38,8 +38,12 @@ def first_challenge
          :status => "alive"
        },
        :heroine_friends => [
-      {name: "Steven", age: "30", attitude: "confused"},
-       {name: "Nurse", age: "44", attitude: "worried"}
+       {name: "Steven",
+        age: "30",
+        attitude: "confused"},
+       {name: "Nurse",
+        age: "44",
+        attitude: "worried"}
      ]
      }
    }
