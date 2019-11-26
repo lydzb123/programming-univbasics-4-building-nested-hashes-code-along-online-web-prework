c3e888d3936d907bb9739f5bba9552f133c92af9
Lydia B <lydiabaik@gmail.com>
2019-11-26 00:43:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,13 +1,9 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriach => {:name => "Lord montague", :age => "53"},
-       :matriarch => {:name => "Lady Montague", :age => "54"},
-       :hero => {
-         :name => "Romeo",
-         :age => "15",
-         :status => "alive"
-       },
+       :patriach => {name: "Lord Montague", age: "53"},
+       :matriarch => {name: "Lady Montague", age: "54"},
+       :hero => {name:"Romeo", age: "15", status: "alive"},
 
      },
      :capulet => {
