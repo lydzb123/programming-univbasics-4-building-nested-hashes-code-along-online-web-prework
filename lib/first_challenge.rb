538de489747b4dfd5e6919954d51b4644416503b
Lydia B <lydiabaik@gmail.com>
2019-11-26 00:18:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,11 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague = {
+       :patriach = {},
+       :matriarch = {},
+       :hero = {},
+       :hero_friends = {}
+     },
+     :capulet = {}
+   }
 end
